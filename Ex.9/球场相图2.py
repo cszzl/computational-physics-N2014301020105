770dbb27b2f500c886529b50d941d9ef1d861cd0
cszzl <15927563415@163.com>
2016-11-21 08:18:21 +0800

    Add files via upload

---
+++ Ex.9/球场相图2.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 20 22:35:41 2016
+
+@author: 赵晓胜
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+class billiard_circle():
+    def __init__(self,x_0,y_0,vx_0,vy_0,N,dt,alpha):
+        self.x_0 = x_0
+        self.y_0 = y_0
+        self.vx_0 = vx_0
+        self.vy_0 = vy_0
+        self.N = N
+        self.dt = dt
+        self.alpha=alpha
+    def motion_calculate(self):
+        self.x = []
+        self.y = []
+        self.vx = []
+        self.vy = []
+        self.t = [0]
+        self.x.append(self.x_0)
+        self.y.append(self.y_0)
+        self.vx.append(self.vx_0)
+        self.vy.append(self.vy_0)
+        for i in range(1,self.N):
+            self.x.append(self.x[i - 1] + self.vx[i - 1]*self.dt)
+            self.y.append(self.y[i - 1] + self.vy[i - 1]*self.dt)
+            self.vx.append(self.vx[i - 1])
+            self.vy.append(self.vy[i - 1])
+            if (np.sqrt( self.x[i]**2+(self.y[i]-self.alpha)**2 ) > 1.0) and self.y[i]>self.alpha:
+                self.x[i],self.y[i] = self.correct('np.sqrt(x**2+(y-self.alpha)**2) < 1.0',self.x[i - 1], self.y[i - 1], self.vx[i - 1], self.vy[i - 1])
+                self.vx[i],self.vy[i] = self.reflect1(self.x[i],self.y[i],self.vx[i - 1], self.vy[i - 1])
+            elif (np.sqrt( self.x[i]**2+(self.y[i]+self.alpha)**2 ) > 1.0) and self.y[i]<-self.alpha:
+                self.x[i],self.y[i] = self.correct('np.sqrt(x**2+(y+self.alpha)**2) < 1.0',self.x[i - 1], self.y[i - 1], self.vx[i - 1], self.vy[i - 1])
+                self.vx[i],self.vy[i] = self.reflect2(self.x[i],self.y[i],self.vx[i - 1], self.vy[i - 1])
+            elif (self.x[i] < -1.0) and self.y[i]>-self.alpha and self.y[i]<self.alpha:
+                self.x[i],self.y[i] = self.correct('x>-1.0',self.x[i - 1], self.y[i - 1], self.vx[i - 1], self.vy[i - 1])
+                self.vx[i] = - self.vx[i]
+            elif (self.x[i] > 1.0) and self.y[i]>-self.alpha and self.y[i]<self.alpha:
+                self.x[i],self.y[i] = self.correct('x<1.0',self.x[i - 1], self.y[i - 1], self.vx[i - 1], self.vy[i - 1])
+                self.vx[i] = - self.vx[i]
+            self.t.append(self.t[i - 1] + self.dt)
+              
+        return self.x, self.y
+        
+    def correct(self,condition,x,y,vx,vy):
+        vx_c = vx/100.0
+        vy_c = vy/100.0
+        while eval(condition):
+            x = x + vx_c*self.dt
+            y = y + vy_c*self.dt
+        return x-vx_c*self.dt,y-vy_c*self.dt    
+    def reflect1(self,x,y,vx,vy):
+        module = np.sqrt(x**2+(y-self.alpha)**2)  ### normalization
+        x = x/module
+        y = (y-self.alpha)/module+self.alpha
+        v = np.sqrt(vx**2+vy**2)
+        cos1 = (vx*x+vy*(y-self.alpha))/v
+        cos2 = (vx*(y-self.alpha)-vy*x)/v
+        vt = -v*cos1
+        vc = v*cos2 
+        vx_n = vt*x+vc*(y-self.alpha)
+        vy_n = vt*(y-self.alpha)-vc*x
+        return vx_n,vy_n
+    
+    def reflect2(self,x,y,vx,vy):
+        module = np.sqrt(x**2+(y+self.alpha)**2)  ### normalization
+        x = x/module
+        y = (y+self.alpha)/module-self.alpha
+        v = np.sqrt(vx**2+vy**2)
+        cos1 = (vx*x+vy*(y+self.alpha))/v
+        cos2 = (vx*(y+self.alpha)-vy*x)/v
+        vt = -v*cos1
+        vc = v*cos2 
+        vx_n = vt*x+vc*(y+self.alpha)
+        vy_n = vt*(y+self.alpha)-vc*x
+        return vx_n,vy_n
+        
+    def plot(self):
+        plt.xlim(-1,1)
+        plt.ylim(-1,1)
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('Stadium billiard $\\alpha$=0.01')
+        self.plot_boundary()
+        plt.plot(self.x,self.y,'y')
+
+        plt.show()
+
+    def phase_plot(self):
+        record_x = []
+        record_vx = []
+        for i in range(len(self.x)):
+            if (abs(self.y[i] - 0)<0.001):
+                record_vx.append(self.vx[i])
+                record_x.append(self.x[i])
+    
+        plt.xlabel('x')
+        plt.ylabel('$v_x$')
+        plt.title('$\\alpha=0.1$')
+        plt.plot(record_x, record_vx,'r.')
+        plt.show()
+
+
+
+A=billiard_circle(0.2,0,1,0.6,500000,0.01,0.1)
+A.motion_calculate()
+A.phase_plot()
+plt.show()
